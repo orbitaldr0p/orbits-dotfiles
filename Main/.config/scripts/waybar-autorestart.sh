646e66151cd6fd9c78e0217c899f1557cb465a63
_orbitaldrop <githubonly@proton.me>
2023-11-24 21:21:33 +0000

    Stuff

---
+++ Main/.config/scripts/waybar-autorestart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Kill and restart waybar whenever its config files change
+CONFIG_FILES="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css"
+trap "killall waybar" EXIT
+
+while true; do
+    waybar &
+    inotifywait -e create,modify $CONFIG_FILES
+    killall waybar
+done
