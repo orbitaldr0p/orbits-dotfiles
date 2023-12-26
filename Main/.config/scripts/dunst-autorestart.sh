44b6472507a0f41b089b2b7ee2b824af48c879d4
_orbitaldrop <githubonly@proton.me>
2023-12-26 09:54:27 +0800

    modified script

--- Main/.config/scripts/dunst-autorestart.sh
+++ Main/.config/scripts/dunst-autorestart.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# Kill and restart waybar whenever its config files change
+# Kill and restart dunst whenever its config files change
 CONFIG_FILES="$HOME/.config/dunst/dunstrc"
 trap "killall dunst" EXIT
 
+first_run=true
+
 while true; do
     dunst &
-    dunstify "Dunst Started"
+    if [ "$first_run" = true ]; then
+        dunstify "Dunst Started"
+        first_run=false
+    else
+        dunstify "Dunst Reloaded"
+    fi
     inotifywait -e create,modify $CONFIG_FILES
     killall dunst
 done
