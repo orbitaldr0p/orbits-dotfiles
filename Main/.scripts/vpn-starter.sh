9f387f3acc1a2c5cac364acfe838eddd3db6f63a
_orbitaldrop <githubonly@proton.me>
2024-02-19 22:51:22 +0000

    REEEEEEEEEEEEEEEEE

--- Main/.scripts/vpn-starter.sh
+++ Main/.scripts/vpn-starter.sh
@@ -10,10 +10,11 @@ target_timezone="Asia/Shanghai"
 # Check if the current timezone is Shanghai
 if [ "$current_timezone" == "$target_timezone" ]; then
     echo "Launching Clash"
-    sleep 3
+    sleep 5
     cfw
 else
     echo "Launching Mullvad"
-    sleep 3
+    mullvad
+    sleep 5
     mullvad-vpn
 fi
