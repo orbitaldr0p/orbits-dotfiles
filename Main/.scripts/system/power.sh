f8c093ab7ff1230eac9f717280e233d2fcc92e3a
_orbitaldrop <githubonly@proton.me>
2024-03-10 00:34:17 +0000

    hmmm

---
+++ Main/.scripts/system/power.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Check if no argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [e|s|r]"
+    exit 1
+fi
+
+# Check the provided argument
+case "$1" in
+    e)
+        screenpad off
+        for ((i=3; i>=1; i--)); do
+            dunstify "Logging out in $i seconds..." -t 1000 -r 91190
+            sleep 1
+        done
+        hyprctl dispatch exit
+        ;;
+    s)
+        screenpad off
+        for ((i=3; i>=1; i--)); do
+            dunstify "Shutting Down in $i seconds..." -t 1000 -r 91190
+            sleep 1
+        done
+        shutdown now
+        ;;
+    r)
+        screenpad off
+        for ((i=3; i>=1; i--)); do
+            dunstify "Restarting in $i seconds..." -t 1000 -r 91190
+            sleep 1
+        done
+        systemctl reboot
+        ;;
+    *)
+        echo "Invalid option: $1. Please provide e (logout), s (shutdown), or r (restart)."
+        exit 1
+        ;;
+esac
+
+exit 0
