4aec71955d56ee2fdb420b4c16e42960f4eb2a89
orbitaldr0p <githubonly@proton.me>
2024-04-07 21:43:50 +0100

    breaking changes woooo

--- Main/.config/fabric/bar.py
+++ Main/.config/fabric/bar.py
@@ -1,42 +1,48 @@
-import math
-import os
-import subprocess
-
+"""desktop status bar example"""
 import fabric
-from fabric.audio.service import Audio
-from fabric.hyprland.widgets import ActiveWindow, Language, WorkspaceButton, Workspaces
-from fabric.system_tray import SystemTray
-from fabric.utils.fabricator.fabricator import Fabricate
-from fabric.utils.helpers import (
-    bulk_replace,
-    invoke_repeater,
-    monitor_file,
-    set_stylesheet_from_file,
-)
-from fabric.utils.string_formatter import FormattedString
-from fabric.widgets.box import Box
-from fabric.widgets.centerbox import CenterBox
-from fabric.widgets.circular_progress_bar import CircularProgressBar
-from fabric.widgets.date_time import DateTime
-from fabric.widgets.eventbox import EventBox
-from fabric.widgets.label import Label
-from fabric.widgets.overlay import Overlay
-from fabric.widgets.wayland import Window
-from loguru import logger
+import os
 import psutil
+from loguru import logger
+from fabric.widgets.box import Box
+from fabric.widgets.label import Label
+from fabric.system_tray import SystemTray
+from fabric.widgets.wayland import Window
+from fabric.widgets.overlay import Overlay
+from fabric.widgets.eventbox import EventBox
+from fabric.widgets.date_time import DateTime
+from fabric.widgets.centerbox import CenterBox
+from fabric.utils.string_formatter import FormattedString
+from fabric.widgets.circular_progress_bar import CircularProgressBar
+from fabric.hyprland.widgets import WorkspaceButton, Workspaces, ActiveWindow, Language
+from fabric.utils import (
+    set_stylesheet_from_file,
+    bulk_replace,
+    monitor_file,
+    invoke_repeater,
+    get_relative_path,
+)
+
+PYWAL = False
+AUDIO_WIDGET = True
+
+if AUDIO_WIDGET is True:
+    try:
+        from fabric.audio.service import Audio
+    except Exception as e:
+        logger.error(e)
+        AUDIO_WIDGET = False
+
 
 class VolumeWidget(Box):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.audio = Audio()
-        self.audioLabel = ""
 
         self.circular_progress_bar = CircularProgressBar(
             name="volume-circular-progress-bar",
             background_color=False,  # false = disabled
             radius_color=False,
-            pie=False,
-            line_width=4
+            pie=True,
         )
 
         self.event_box = EventBox(
@@ -44,34 +50,28 @@ class VolumeWidget(Box):
             children=Overlay(
                 children=self.circular_progress_bar,
                 overlays=Label(
-                    label=self.audioLabel,
-                    style="margin: 0px 0px 0px 0px; font-size: 12px"
+                    label="",
+                    style="margin: 0px 6px 0px 0px; font-size: 12px",  # because glyph icon is not centered
                 ),
             ),
         )
 
         self.event_box.connect("scroll-event", self.on_scroll)
-        self.event_box.connect("button-press-event", self.on_click)
         self.audio.connect("speaker-changed", self.update)
         self.add(self.event_box)
 
     def on_scroll(self, widget, event):
         if event.direction == 0:
-            self.audio.speaker.volume += 10
-            os.system(f'dunstify "Volume: {math.ceil(self.audio.speaker.volume)}" -t 800 -r 91190')
+            self.audio.speaker.volume += 8
         elif event.direction == 1:
-            self.audio.speaker.volume -= 10
-            os.system(f'dunstify "Volume: {math.ceil(self.audio.speaker.volume)}" -t 800 -r 91190')
+            self.audio.speaker.volume -= 8
 
-    def on_click(self, widget, event):
-        if event.button == 1:
-            os.system('pavucontrol')
-            
     def update(self, *args):
         if self.audio.speaker is None:
             return
         self.circular_progress_bar.percentage = self.audio.speaker.volume
         return
+
 
 class StatusBar(Window):
     def __init__(
@@ -80,21 +80,24 @@ class StatusBar(Window):
         super().__init__(
             layer="top",
             anchor="left top right",
-            margin="7px 7px 0px 7px",
+            margin="10px 10px -2px 10px",
             exclusive=True,
-            visible=True,
+            visible=False,
+            all_visible=False,
         )
         self.center_box = CenterBox(name="main-window")
         self.workspaces = Workspaces(
-            buttons_list=[
-                WorkspaceButton(label=FormattedString("1")),
-                WorkspaceButton(label=FormattedString("2")),
-                WorkspaceButton(label=FormattedString("3")),
-                WorkspaceButton(label=FormattedString("4")),
-                WorkspaceButton(label=FormattedString("5")),
-            ],
-            spacing=6,
+            spacing=2,
             name="workspaces",
+            buttons_list=[
+                WorkspaceButton(label=FormattedString("")),
+                WorkspaceButton(label=FormattedString("")),
+                WorkspaceButton(label=FormattedString("")),
+                WorkspaceButton(label=FormattedString("")),
+                WorkspaceButton(label=FormattedString("")),
+                WorkspaceButton(label=FormattedString("")),
+                WorkspaceButton(label=FormattedString("")),
+            ],
         )
         self.active_window = ActiveWindow(
             formatter=FormattedString(
@@ -107,118 +110,84 @@ class StatusBar(Window):
             ),
             name="hyprland-window",
         )
-
-        self.cpu_label = Label(label="0")
-        self.cpu_freq_label = Label(label="0")
-        self.memory_label = Label(label="0")
-        self.battery_label = Label(label="0")
-        self.system_info_var = Fabricate(
-            value={"ram": 0, "cpu": 0},
-            poll_from=lambda _: {
-                "ram": str(int(psutil.virtual_memory().percent)),
-                "cpu": str(int(psutil.cpu_percent())),
-                "cpu-freq": str(round(float(str(psutil.cpu_freq()).split("current=")[1].split(",")[0]) / 1000, 2)),
-                "battery": str(int(psutil.sensors_battery().percent) if psutil.sensors_battery() is not None else 42),
-            },
-            interval=1000,
-        )
-        self.system_info_var.connect(
-            "changed",
-            lambda _, value: (
-                self.cpu_label.set_label(value["cpu"]),
-                self.memory_label.set_label(value["ram"]),
-                self.battery_label.set_label(value["battery"]),
-                self.cpu_freq_label.set_label(value["cpu-freq"]),
+        self.language = Language(
+            formatter=FormattedString(
+                "{replace_lang(language)}",
+                replace_lang=lambda x: bulk_replace(
+                    x,
+                    [r".*Eng.*", r".*Ar.*"],
+                    ["ENG", "ARA"],
+                    regex=True,
+                ),
             ),
+            name="hyprland-window",
         )
-
-        self.volume = VolumeWidget()
-
-        #Left Widgets
         self.date_time = DateTime(name="date-time")
         self.system_tray = SystemTray(name="system-tray")
-        self.center_box.add_left(self.workspaces)
-        self.center_box.add_left(
-            Box(
-                orientation="h",
-                style="min-width: calc(44px - 8px); margin: 8px;",
-                children=[
-                    Box(
-                        spacing=4,
-                        orientation="h",
-                        children=[
-                            Label(label=""),
-                            self.battery_label,
-                            Label(label="%"),
-                        ],
-                    ),
-                    Box(name="module-separator"),
-                    Box(
-                        spacing=4,
-                        orientation="h",
-                        children=[
-                            Label(label=""),
-                            self.memory_label,
-                            Label(label="%"),
-                        ],
-                    ),
-                    Box(name="module-separator"),
-                    Box(
-                        spacing=4,
-                        orientation="h",
-                        children=[
-                            Label(label=""),
-                            self.cpu_label,
-                            Label(label="%"),
-                            self.cpu_freq_label,
-                            Label(label="GHz")
-                        ],
-                    ),
-                ],
-            )
+        self.ram_circular_progress_bar = CircularProgressBar(
+            name="ram-circular-progress-bar",
+            background_color=False,  # false = disabled
+            radius_color=False,
+            pie=True,
         )
+        self.cpu_circular_progress_bar = CircularProgressBar(
+            name="cpu-circular-progress-bar",
+            background_color=False,
+            radius_color=False,
+            pie=True,
+        )
+        self.circular_progress_bars_overlay = Overlay(
+            children=self.ram_circular_progress_bar,
+            overlays=[
+                self.cpu_circular_progress_bar,
+                Label("", style="margin: 0px 6px 0px 0px; font-size: 12px"),
+            ],
+        )
+        self.volume = VolumeWidget() if AUDIO_WIDGET is True else None
+        self.widgets_container = Box(
+            spacing=2,
+            orientation="h",
+            name="widgets-container",
+            children=[
+                self.circular_progress_bars_overlay,
+            ],
+        )
+        self.widgets_container.add(self.volume) if self.volume is not None else None
 
-        #Center Widgets
+        self.center_box.add_start(self.workspaces)
         self.center_box.add_center(self.active_window)
-        
-        #Right Widgets
-        self.center_box.add_right(
-            Box(
-                orientation="h",
-                style="min-width: calc(44px - 8px); margin: 8px;",
-                children=[
-                    Box(
-                        spacing=4,
-                        orientation="h",
-                        children=[
-                            self.system_tray
-                        ],
-                    ),
-                    Box(name="module-separator"),
-                    Box(
-                        spacing=4,
-                        orientation="h",
-                        children=[
-                            self.volume
-                        ],
-                    ),
-                    Box(name="module-separator"),
-                    Box(
-                        spacing=4,
-                        orientation="h",
-                        children=[
-                            self.date_time
-                        ],
-                    ),
-                ],
-            )
-        )
-        
+        self.center_box.add_end(self.widgets_container)
+        self.center_box.add_end(self.system_tray)
+        self.center_box.add_end(self.date_time)
+        self.center_box.add_end(self.language)
         self.add(self.center_box)
+
+        invoke_repeater(1000, self.update_progress_bars)
+        self.update_progress_bars()  # initial call
+
         self.show_all()
+
+    def update_progress_bars(self):
+        self.ram_circular_progress_bar.percentage = psutil.virtual_memory().percent
+        self.cpu_circular_progress_bar.percentage = psutil.cpu_percent()
+        return True
+
+
+def apply_style(*args):
+    logger.info("[Bar] CSS applied")
+    return set_stylesheet_from_file(get_relative_path("bar.css"))
 
 
 if __name__ == "__main__":
-    StatusBar()
-    set_stylesheet_from_file('.config/fabric/bar.css')
-    fabric.start()
+    bar = StatusBar()
+
+    if PYWAL is True:
+        monitor = monitor_file(
+            f"/home/{os.getlogin()}/.cache/wal/colors-widgets.css", "none"
+        )
+        monitor.connect("changed", apply_style)
+
+    # initialize style
+    apply_style()
+
+    fabric.start(open_client=False)
