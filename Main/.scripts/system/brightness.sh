ccdc1b38e75cccb44252db516c340bf1c6912b44
orbitaldr0p <githubonly@proton.me>
2024-04-08 09:17:05 +0100

    hmmm

--- Main/.scripts/system/brightness.sh
+++ Main/.scripts/system/brightness.sh
@@ -3,23 +3,14 @@
 increase() {
   brightnessctl s +10%
   brightness=$(brightnessctl get)
-  #screenpad
-  dunstify "Brightness: $(( $brightness * 100 / 255 ))" -t 800 -r 91170
+  dunstify "Brightness: $(( $brightness * 100 / 2047 ))" -t 800 -r 91170
 }
 
 decrease() {
   brightnessctl s 10%-
   brightness=$(brightnessctl get)
-  #screenpad
-  dunstify "Brightness: $(( $brightness * 100 / 255 ))" -t 800 -r 91170
+  dunstify "Brightness: $(( $brightness * 100 / 2047 ))" -t 800 -r 91170
 }
-
-#Too Inefficent
-#screenpad() {
-#  if [ "$brightness" > 0 ] ; then
-#    echo "$brightness" | tee '/sys/class/leds/asus::screenpad/brightness'
-#  fi
-#}
 
 invalid_input(){
   echo lmao
