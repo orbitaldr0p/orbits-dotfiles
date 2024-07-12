e04ebf295b05c5db3de0d90e3826c09c6bcb9724
orbitaldr0p <githubonly@proton.me>
2024-07-12 22:00:03 +0100

    added dunst vol icons

--- Main/.scripts/system/volume-control.sh
+++ Main/.scripts/system/volume-control.sh
@@ -4,23 +4,27 @@ icondir="$HOME/.resources/dunst/volume/"
 volume_increased() {
   wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0
   vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oE '[0-9]+\.[0-9]+' | awk '{printf "%.0f\n", $1 * 100}')
-  dunstify "Volume: $vol%" -t 800 -r 91190 -i "lol"
+  icon=$(get_icon "$vol")
+  dunstify "Volume: $vol%" -t 800 -r 91190 -i "${icon}"
 }
 
 volume_decreased() {
   wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0
   vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oE '[0-9]+\.[0-9]+' | awk '{printf "%.0f\n", $1 * 100}')
-  dunstify "Volume: $vol%" -t 800 -r 91190 -i "lol"
+  icon=$(get_icon "$vol")
+  dunstify "Volume: $vol%" -t 800 -r 91190 -i "${icon}"
 }
 
 muted() {
   muteState=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
   if echo "$muteState" | grep -q '\[MUTED\]'; then
     wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-    dunstify "Unmuted" -t 800 -r 91190
+    icon=$(get_icon "$vol")
+    dunstify "Unmuted" -t 800 -r 91190 -i "${icon}"
   else
     wpctl set-mute @DEFAULT_AUDIO_SINK@ 1
-    dunstify "Muted" -t 800 -r 91190
+    icon="${icondir}/vol-muted.svg"
+    dunstify "Muted" -t 800 -r 91190 -i "${icon}"
   fi
 }
 
@@ -34,6 +38,16 @@ mic_mute() {
     wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 1
     icon="${icondir}/mic-muted.svg"
     dunstify "Mic Muted" -t 800 -r 91190 -i "${icon}"
+  fi
+}
+
+get_icon() {
+  if [ "$1" -le 33 ]; then
+    echo "${icondir}/vol-0.svg"
+  elif [ "$1" -le 66 ]; then
+    echo "${icondir}/vol-1.svg"
+  else
+    echo "${icondir}/vol-2.svg"
   fi
 }
 
