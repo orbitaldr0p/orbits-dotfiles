f83ca862303caaaee93def9771653ed03cc7b8fd
orbitaldr0p <githubonly@proton.me>
2024-08-03 18:29:43 +0100

    icons for power profile script :thumbsup:

--- Main/.scripts/System/powerProfile.sh
+++ Main/.scripts/System/powerProfile.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+icondir="$HOME/.resources/dunst/power/"
 currentProfile=$(powerprofilesctl get)
 case $currentProfile in
     power-saver)
-        dunstify "Profile: Balanced" -t 800 -r 91160
         powerprofilesctl set balanced
+        icon="${icondir}/balanced.svg"
+        dunstify "Profile: Balanced" -t 800 -r 91160 -i "${icon}"
     ;;
     
     balanced)
-        dunstify "Profile: Performance" -t 800 -r 91160
         powerprofilesctl set performance
+        icon="${icondir}/performance.svg"
+        dunstify "Profile: Performance" -t 800 -r 91160 -i "${icon}"
     ;;
     
     performance)
-        dunstify "Profile: Silent" -t 800 -r 91160
         powerprofilesctl set power-saver
-        
+        icon="${icondir}/silent.svg"
+        dunstify "Profile: Silent" -t 800 -r 91160 -i "${icon}"
     ;;
 esac
