d1252ceba666f56306c7f2220aaab5db9cc38abe
orbitaldr0p <githubonly@proton.me>
2025-03-19 16:18:39 +0000

    seperated waybar configs for niri & hypr

--- Main/.scripts/AutoRestart/waybarAutorestart.sh
+++ Main/.scripts/AutoRestart/waybarAutorestart.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
-# Kill and restart waybar whenever its config files change
-CONFIG_FILES="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css"
-trap "killall waybar" EXIT
+hypr() {
+    # Kill and restart waybar whenever its config files change
+    CONFIG_FILES="$HOME/.config/waybar/hypr/config.jsonc $HOME/.config/waybar/hypr/style.css"
+    trap "killall waybar" EXIT
+    while true; do
+        waybar -c $HOME/.config/waybar/hypr/config.jsonc &
+        inotifywait -e create,modify $CONFIG_FILES
+        sleep 1
+        killall waybar
+    done
+}
 
-while true; do
-    waybar &
-    inotifywait -e create,modify $CONFIG_FILES
-    sleep 1
-    killall waybar
-done
+
+niri () {
+    # Kill and restart waybar whenever its config files change
+    CONFIG_FILES="$HOME/.config/waybar/niri/config.jsonc $HOME/.config/waybar/niri/style.css"
+    trap "killall waybar" EXIT
+    while true; do
+        waybar -c $HOME/.config/waybar/niri/config.jsonc &
+        inotifywait -e create,modify $CONFIG_FILES
+        sleep 1
+        killall waybar
+    done
+}
+
+case "$1" in
+    h)
+        hypr
+    ;;
+    n)
+        niri
+    ;;
+    *)
+        invalid_input
+    ;;
+esac
