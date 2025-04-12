99db1a7c8d9d284b10f30af8c2aa97c27542a609
orbitaldr0p <githubonly@proton.me>
2025-04-12 22:32:41 +0100

    oop

--- Main/.scripts/System/volumeControl.sh
+++ Main/.scripts/System/volumeControl.sh
@@ -1,76 +1,76 @@
 #!/bin/bash
 icondir="$HOME/.resources/notifications/volume/"
 
-volume_increased() {
+volIncrease() {
     wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0
     vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oE '[0-9]+\.[0-9]+' | awk '{printf "%.0f\n", $1 * 100}')
-    icon=$(get_icon "$vol")
-    notify-send "Volume: $vol%" -t 800 -r 91190 -i "${icon}"
+    icon=$(getIcon $vol)
+    notify-send "Volume: $vol%" -t 800 -r 91190 -i "$icon"
 }
 
-volume_decreased() {
+volDecrease() {
     wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0
     vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oE '[0-9]+\.[0-9]+' | awk '{printf "%.0f\n", $1 * 100}')
-    icon=$(get_icon "$vol")
-    notify-send "Volume: $vol%" -t 800 -r 91190 -i "${icon}"
+    icon=$(getIcon $vol)
+    notify-send "Volume: $vol%" -t 800 -r 91190 -i "$icon"
 }
 
-muted() {
+volMute() {
     muteState=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
     if echo "$muteState" | grep -q '\[MUTED\]'; then
         wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-        icon=$(get_icon "$vol")
-        notify-send "Unmuted" -t 800 -r 91190 -i "${icon}"
+        icon=$(getIcon "$vol")
+        notify-send "Unmuted" -t 800 -r 91190 -i "$icon"
     else
         wpctl set-mute @DEFAULT_AUDIO_SINK@ 1
-        icon="${icondir}/vol-0.png"
-        notify-send "Muted" -t 800 -r 91190 -i "${icon}"
+        icon="$icondir/vol-0.png"
+        notify-send "Muted" -t 800 -r 91190 -i "$icon"
     fi
 }
 
-mic_mute() {
+micMute() {
     muteState=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@)
     if echo "$muteState" | grep -q '\[MUTED\]'; then
         wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0
-        icon="${icondir}/mic-1.png"
+        icon="$icondir/mic-1.png"
         notify-send "Mic Unmuted" -t 800 -r 91190 -i "${icon}"
     else
         wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 1
-        icon="${icondir}/mic-0.png"
+        icon="$icondir/mic-0.png"
         notify-send "Mic Muted" -t 800 -r 91190 -i "${icon}"
     fi
 }
 
-get_icon() {
+getIcon() {
     if [ "$1" -eq 0 ]; then
-        echo "${icondir}/vol-1.png"
+        echo "$icondir/vol-1.png"
     elif [ "$1" -le 34 ]; then
-        echo "${icondir}/vol-2.png"
+        echo "$icondir/vol-2.png"
     elif [ "$1" -le 66 ]; then
-        echo "${icondir}/vol-3.png"
+        echo "$icondir/vol-3.png"
     else
-        echo "${icondir}/vol-4.png"
+        echo "$icondir/vol-4.png"
     fi
 }
 
-invalid_input() {
+invalidInput() {
     echo lmao
 }
 
 case "$1" in
     i)
-        volume_increased
+        volIncrease
     ;;
     d)
-        volume_decreased
+        volDecrease
     ;;
     m)
-        muted
+        volMute
     ;;
     mm)
-        mic_mute
+        micMute
     ;;
     *)
-        invalid_input
+        invalidInput
     ;;
 esac
