7798311b26c9c27453b5d277b65274443409b313
orbitaldr0p <githubonly@proton.me>
2025-05-02 12:09:24 +0100

    stuff

--- Main/.config/ags/app.ts
+++ Main/.config/ags/app.ts
@@ -1,10 +1,10 @@
 import { App } from "astal/gtk4"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
+import style from "./widget/rightPanel/style.scss"
+import rightPanel from "./widget/rightPanel/rightPanel"
 
 App.start({
     css: style,
     main() {
-        App.get_monitors().map(Bar)
+        App.get_monitors().map(rightPanel)
     },
 })
